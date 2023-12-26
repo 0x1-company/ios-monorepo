303e15e0aec292273b7e59ccb58183c11f2008de
tomokisun <github@tomokisun.com>
2023-12-26 09:03:42 +0900

    feat: 🎸 start recording

--- Packages/FlyCam/Sources/CameraRecordFeature/CameraRecord.swift
+++ Packages/FlyCam/Sources/CameraRecordFeature/CameraRecord.swift
@@ -2,6 +2,7 @@ import AnalyticsClient
 import AVFoundation
 import ComposableArchitecture
 import CoreMotion
+import FeedbackGeneratorClient
 import SwiftUI
 
 private let GRAVITY_THRESHOLD = 0.2
@@ -16,6 +17,7 @@ public struct CameraRecordLogic {
     var zeroGravityStartTime: Date?
     var zeroGravityEndTime: Date?
     
+    var isRecording = false
     var isActivityIndicatorVisible = false
     
     var videoCamera: VideoCameraLogic.State?
@@ -35,8 +37,10 @@ public struct CameraRecordLogic {
 
   public enum Action {
     case onTask
+    case startRecordButtonTapped
     case accelerometerUpdates(Result<CMAccelerometerData, Error>)
-    case showResultDelayCompleted(Double)
+    case stopRecordDelayCompleted
+    case showResultDelayCompleted
     case videoCamera(VideoCameraLogic.Action)
     case delegate(Delegate)
 
@@ -48,57 +52,71 @@ public struct CameraRecordLogic {
   @Dependency(\.date.now) var now
   @Dependency(\.analytics) var analytics
   @Dependency(\.continuousClock) var clock
+  @Dependency(\.feedbackGenerator) var feedbackGenerator
+  
+  enum Cancel {
+    case accelerometerUpdates
+  }
 
   public var body: some Reducer<State, Action> {
     Reduce<State, Action> { state, action in
       switch action {
       case .onTask:
-        let motionManager = state.motionManager
+        return .none
 
-        guard motionManager.isAccelerometerAvailable
-        else { return .none }
+      case .startRecordButtonTapped:
+        let motionManager = state.motionManager
         motionManager.accelerometerUpdateInterval = 0.01
 
+        let delegate = Delegate()
+        state.videoCamera?.fileOutput.startRecording(to: state.videoFileURL, recordingDelegate: delegate)
+        state.isRecording = true
+        
         return .run { send in
+          await feedbackGenerator.impactOccurred()
           for await result in startAccelerometerUpdates(motionManager) {
             await send(.accelerometerUpdates(result))
           }
         }
+        .cancellable(id: Cancel.accelerometerUpdates, cancelInFlight: true)
 
       case let .accelerometerUpdates(.success(data)):
         let acceleration = data.acceleration
         let accelerationMagnitude = sqrt(acceleration.x * acceleration.x + acceleration.y * acceleration.y + acceleration.z * acceleration.z)
 
         if accelerationMagnitude < GRAVITY_THRESHOLD {
-          if state.zeroGravityStartTime == nil {
-            state.zeroGravityStartTime = now
-            let delegate = Delegate()
-            state.videoCamera?.fileOutput.startRecording(to: state.videoFileURL, recordingDelegate: delegate)
-          }
+          guard state.zeroGravityStartTime == nil else { return .none }
+          state.zeroGravityStartTime = now
+          return .none
         } else {
           guard state.zeroGravityStartTime != nil else { return .none }
           state.isActivityIndicatorVisible = true
           state.zeroGravityEndTime = now
           state.motionManager.stopAccelerometerUpdates()
-          state.videoCamera?.fileOutput.stopRecording()
-          state.videoCamera?.captureSession.stopRunning()
-
-          guard
-            let startTime = state.zeroGravityStartTime,
-            let endTime = state.zeroGravityEndTime
-          else { return .none }
-          let zeroGravityTime = endTime.timeIntervalSince(startTime)
-          let altitude = calculateAltitude(timeInZeroGravitySeconds: zeroGravityTime)
 
           return .run { send in
-            try await self.clock.sleep(for: .seconds(2))
-            await send(.showResultDelayCompleted(altitude))
+            try await self.clock.sleep(for: .seconds(1))
+            await send(.stopRecordDelayCompleted)
           }
         }
-        return .none
         
-      case let .showResultDelayCompleted(altitude):
+      case .stopRecordDelayCompleted:
+        state.videoCamera?.fileOutput.stopRecording()
+        state.videoCamera?.captureSession.stopRunning()
+
+        return .run { send in
+          try await self.clock.sleep(for: .seconds(2))
+          await send(.showResultDelayCompleted)
+        }
+        
+      case .showResultDelayCompleted:
         state.isActivityIndicatorVisible = false
+        guard
+          let startTime = state.zeroGravityStartTime,
+          let endTime = state.zeroGravityEndTime
+        else { return .none }
+        let zeroGravityTime = endTime.timeIntervalSince(startTime)
+        let altitude = calculateAltitude(timeInZeroGravitySeconds: zeroGravityTime)
         return .send(.delegate(.result(altitude, state.videoFileURL)))
         
       default:
@@ -160,9 +178,25 @@ public struct CameraRecordView: View {
           }
         }
 
-        Text("Throw the iPhone and start shooting", bundle: .module)
-          .frame(maxHeight: .infinity)
-          .font(.system(.title3, weight: .semibold))
+        VStack(spacing: 12) {
+          Text("Press the button to throw the iPhone", bundle: .module)
+            .frame(maxHeight: .infinity)
+            .font(.system(.title3, weight: .semibold))
+            .scaleEffect(viewStore.isRecording ? 0.0 : 1.0)
+            .animation(.default, value: viewStore.isRecording)
+          
+          Button {
+            store.send(.startRecordButtonTapped)
+          } label: {
+            RoundedRectangle(cornerRadius: 80 / 2)
+              .stroke(Color.white, lineWidth: 6.0)
+              .frame(width: 80, height: 80)
+              .background(Material.ultraThin)
+              .clipShape(RoundedRectangle(cornerRadius: 80 / 2))
+          }
+          .scaleEffect(viewStore.isRecording ? 0.0 : 1.0)
+          .animation(.default, value: viewStore.isRecording)
+        }
       }
       .padding(.vertical, 24)
       .task { await store.send(.onTask).finish() }
