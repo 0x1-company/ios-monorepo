0269b91dbabdd688d6507afd1830e2fc285c87ea
tomokisun <github@tomokisun.com>
2023-12-29 15:54:44 +0900

    feat: 🎸 received like alert

--- Packages/BeMatch/Sources/MatchFeature/ReceivedLikeGrid.swift
+++ Packages/BeMatch/Sources/MatchFeature/ReceivedLikeGrid.swift
@@ -11,6 +11,8 @@ public struct ReceivedLikeGridLogic {
   public struct State: Equatable {
     let imageUrl: String
     let count: Int
+    
+    @PresentationState var alert: AlertState<Action.Alert>?
 
     public init(imageUrl: String, count: Int) {
       self.imageUrl = imageUrl
@@ -18,10 +20,38 @@ public struct ReceivedLikeGridLogic {
     }
   }
 
-  public enum Action {}
+  public enum Action {
+    case gridButtonTapped
+    case alert(PresentationAction<Alert>)
+    
+    public enum Alert: Equatable {
+      case confirmOkay
+    }
+  }
 
   public var body: some Reducer<State, Action> {
-    EmptyReducer()
+    Reduce<State, Action> { state, action in
+      switch action {
+      case .gridButtonTapped:
+        state.alert = AlertState {
+          TextState("Liked by \(state.count) people", bundle: .module)
+        } actions: {
+          ButtonState(action: .confirmOkay) {
+            TextState("OK", bundle: .module)
+          }
+        } message: {
+          TextState("Swipe more to match!", bundle: .module)
+        }
+        return .none
+        
+      case .alert(.presented(.confirmOkay)):
+        state.alert = nil
+        return .none
+        
+      case .alert:
+        return .none
+      }
+    }
   }
 }
 
@@ -46,47 +76,52 @@ public struct ReceivedLikeGridView: View {
 
   public var body: some View {
     WithViewStore(store, observe: { $0 }) { viewStore in
-      VStack(spacing: 0) {
-        CachedAsyncImage(
-          url: URL(string: viewStore.imageUrl),
-          urlCache: .shared,
-          scale: displayScale,
-          content: { image in
-            image
-              .resizable()
-              .aspectRatio(3 / 4, contentMode: .fill)
-              .blur(radius: 18)
-          },
-          placeholder: {
-            ProgressView()
-              .progressViewStyle(CircularProgressViewStyle())
-              .tint(Color.white)
-              .aspectRatio(3 / 4, contentMode: .fill)
-              .background()
+      Button {
+        store.send(.gridButtonTapped)
+      } label: {
+        VStack(spacing: 0) {
+          CachedAsyncImage(
+            url: URL(string: viewStore.imageUrl),
+            urlCache: .shared,
+            scale: displayScale,
+            content: { image in
+              image
+                .resizable()
+                .aspectRatio(3 / 4, contentMode: .fill)
+                .blur(radius: 18)
+            },
+            placeholder: {
+              ProgressView()
+                .progressViewStyle(CircularProgressViewStyle())
+                .tint(Color.white)
+                .aspectRatio(3 / 4, contentMode: .fill)
+                .background()
+            }
+          )
+          .cornerRadius(6)
+          .overlay {
+            RoundedRectangle(cornerRadius: 6)
+              .stroke(goldGradient, lineWidth: 3)
           }
-        )
-        .cornerRadius(6)
-        .overlay {
-          RoundedRectangle(cornerRadius: 6)
-            .stroke(goldGradient, lineWidth: 3)
-        }
-        .overlay {
-          Text(viewStore.count.description)
-            .font(.system(.body, weight: .semibold))
-            .frame(width: 40, height: 40)
-            .foregroundStyle(Color.white)
-            .background(goldGradient)
-            .clipShape(Circle())
-        }
-        .overlay(alignment: .bottom) {
-          Image(ImageResource.receivedLike)
-            .offset(y: 17)
-        }
+          .overlay {
+            Text(viewStore.count.description)
+              .font(.system(.body, weight: .semibold))
+              .frame(width: 40, height: 40)
+              .foregroundStyle(Color.white)
+              .background(goldGradient)
+              .clipShape(Circle())
+          }
+          .overlay(alignment: .bottom) {
+            Image(ImageResource.receivedLike)
+              .offset(y: 17)
+          }
 
-        Text("Liked by \(viewStore.count) people", bundle: .module)
-          .font(.system(.subheadline, weight: .semibold))
-          .frame(maxWidth: .infinity, minHeight: 54, maxHeight: .infinity, alignment: .leading)
+          Text("Liked by \(viewStore.count) people", bundle: .module)
+            .font(.system(.subheadline, weight: .semibold))
+            .frame(maxWidth: .infinity, minHeight: 54, maxHeight: .infinity, alignment: .leading)
+        }
       }
+      .alert(store: store.scope(state: \.$alert, action: \.alert))
     }
   }
 }
