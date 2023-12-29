c46bf1f3ae0c3ec8039703efd4b1a397331234ad
tomokisun <github@tomokisun.com>
2023-12-29 16:10:01 +0900

    feat: 🎸 account delete

---
+++ Packages/FlyCam/Sources/AnalyticsKeys/ButtonClickKeys.swift
@@ -0,0 +1,14 @@
+import AnalyticsClient
+
+public extension ButtonClickKeys {
+  var forceUpdate: String { "force_update" }
+  var like: String { "like" }
+  var nope: String { "nope" }
+  var swipeLike: String { "swipe_like" }
+  var swipeNope: String { "swipe_nope" }
+  var addBeReal: String { "add_bereal" }
+  var close: String { "close" }
+  var notNow: String { "not_now" }
+  var delete: String { "delete" }
+  var share: String { "share" }
+}
