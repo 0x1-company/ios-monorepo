ca565f335422b721b6478140147d12158b4cd1f4
tomokisun <github@tomokisun.com>
2024-01-10 21:36:33 +0900

    feat: 🎸 received count max

--- Packages/BeMatch/Sources/MatchFeature/ReceivedLikeGrid.swift
+++ Packages/BeMatch/Sources/MatchFeature/ReceivedLikeGrid.swift
@@ -11,6 +11,9 @@ public struct ReceivedLikeGridLogic {
   public struct State: Equatable {
     let imageUrl: String
     let count: Int
+    var receivedCount: String {
+      return count > 99 ? "99+" : count.description
+    }
 
     @PresentationState var alert: AlertState<Action.Alert>?
 
@@ -34,7 +37,7 @@ public struct ReceivedLikeGridLogic {
       switch action {
       case .gridButtonTapped:
         state.alert = AlertState {
-          TextState("Liked by \(state.count) people", bundle: .module)
+          TextState("Liked by \(state.receivedCount) people", bundle: .module)
         } actions: {
           ButtonState(action: .confirmOkay) {
             TextState("OK", bundle: .module)
@@ -107,7 +110,7 @@ public struct ReceivedLikeGridView: View {
               .stroke(goldGradient, lineWidth: 3)
           }
           .overlay {
-            Text(viewStore.count.description)
+            Text(viewStore.receivedCount)
               .font(.system(.body, weight: .semibold))
               .frame(width: 40, height: 40)
               .foregroundStyle(Color.white)
@@ -119,7 +122,7 @@ public struct ReceivedLikeGridView: View {
               .offset(y: 17)
           }
 
-          Text("Liked by \(viewStore.count) people", bundle: .module)
+          Text("Liked by \(viewStore.receivedCount) people", bundle: .module)
             .foregroundStyle(Color.primary)
             .font(.system(.subheadline, weight: .semibold))
             .frame(maxWidth: .infinity, minHeight: 54, maxHeight: .infinity, alignment: .leading)
