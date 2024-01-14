1abcb736753e73ef320fcfb331eac2008f3c4a14
tomokisun <github@tomokisun.com>
2024-01-14 19:02:57 +0900

    feat: 🎸 invitation code

--- Packages/BeMatch/Package.swift
+++ Packages/BeMatch/Package.swift
@@ -24,6 +24,7 @@ let package = Package(
     .library(name: "EditProfileFeature", targets: ["EditProfileFeature"]),
     .library(name: "ForceUpdateFeature", targets: ["ForceUpdateFeature"]),
     .library(name: "GenderSettingFeature", targets: ["GenderSettingFeature"]),
+    .library(name: "InvitationCodeFeature", targets: ["InvitationCodeFeature"]),
     .library(name: "LaunchFeature", targets: ["LaunchFeature"]),
     .library(name: "MaintenanceFeature", targets: ["MaintenanceFeature"]),
     .library(name: "MatchedFeature", targets: ["MatchedFeature"]),
@@ -154,6 +155,12 @@ let package = Package(
       .product(name: "FeedbackGeneratorClient", package: "SDK"),
       .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
     ]),
+    .target(name: "InvitationCodeFeature", dependencies: [
+      "Styleguide",
+      "BeMatchClient",
+      "AnalyticsKeys",
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+    ]),
     .target(name: "LaunchFeature", dependencies: [
       "Styleguide",
       .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
@@ -192,6 +199,7 @@ let package = Package(
     .target(name: "MatchNavigationFeature", dependencies: [
       "MatchFeature",
       "SettingsFeature",
+      "InvitationCodeFeature",
     ]),
     .target(name: "MembershipFeature", dependencies: [
       "Styleguide",
@@ -313,3 +321,4 @@ let package = Package(
     ]),
   ]
 )
+
---
+++ Packages/BeMatch/Sources/InvitationCodeFeature/InvitationCode.swift
@@ -0,0 +1,115 @@
+import AnalyticsClient
+import BeMatch
+import BeMatchClient
+import ComposableArchitecture
+import SwiftUI
+import Styleguide
+
+@Reducer
+public struct InvitationCodeLogic {
+  public init() {}
+
+  public struct State: Equatable {
+    var code = ""
+
+    public init() {}
+  }
+
+  public enum Action {
+    case onTask
+    case onAppear
+    case shareInvitationCodeButtonTapped
+    case invitationCodeResponse(Result<BeMatch.InvitationCodeQuery.Data, Error>)
+  }
+
+  @Dependency(\.bematch) var bematch
+  @Dependency(\.analytics) var analytics
+  
+  enum Cancel {
+    case invitationCode
+  }
+
+  public var body: some Reducer<State, Action> {
+    Reduce<State, Action> { state, action in
+      switch action {
+      case .onTask:
+        return .run { send in
+          for try await data in bematch.invitationCode() {
+            await send(.invitationCodeResponse(.success(data)))
+          }
+        } catch: { error, send in
+          await send(.invitationCodeResponse(.failure(error)))
+        }
+        .cancellable(id: Cancel.invitationCode, cancelInFlight: true)
+
+      case .onAppear:
+        analytics.logScreen(screenName: "InvitationCode", of: self)
+        return .none
+        
+      case .shareInvitationCodeButtonTapped:
+        return .none
+        
+      case let .invitationCodeResponse(.success(data)):
+        state.code = data.invitationCode.code
+        return .none
+        
+      default:
+        return .none
+      }
+    }
+  }
+}
+
+public struct InvitationCodeView: View {
+  let store: StoreOf<InvitationCodeLogic>
+
+  public init(store: StoreOf<InvitationCodeLogic>) {
+    self.store = store
+  }
+
+  public var body: some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      VStack(spacing: 16) {
+        VStack(spacing: 16) {
+          Image(ImageResource.inviteTicket)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .overlay(alignment: .center) {
+              Text(viewStore.code)
+                .foregroundStyle(Color(0xFFFF_CC00))
+                .font(.system(.largeTitle, design: .rounded, weight: .bold))
+                .offset(x: -35, y: 8)
+            }
+
+          PrimaryButton(
+            String(localized: "Share Invitation Code", bundle: .module)
+          ) {
+            store.send(.shareInvitationCodeButtonTapped)
+          }
+        }
+        .padding(.all, 16)
+        .background(Color(uiColor: UIColor.secondarySystemBackground))
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+        .padding(.horizontal, 16)
+      }
+      .padding(.vertical, 24)
+      .background()
+      .navigationTitle(String(localized: "Invitation Code", bundle: .module))
+      .navigationBarTitleDisplayMode(.inline)
+      .task { await store.send(.onTask).finish() }
+      .onAppear { store.send(.onAppear) }
+    }
+  }
+}
+
+#Preview {
+  NavigationStack {
+    InvitationCodeView(
+      store: .init(
+        initialState: InvitationCodeLogic.State(),
+        reducer: { InvitationCodeLogic() }
+      )
+    )
+  }
+  .environment(\.colorScheme, .dark)
+}
