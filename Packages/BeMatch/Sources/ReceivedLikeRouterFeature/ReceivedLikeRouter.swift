3bc5e6547b8d351882fce85a4a1d28aa4475b39e
tomokisun <github@tomokisun.com>
2024-03-23 06:13:43 +0900

    feat: 🎸 router

--- Packages/BeMatch/Sources/ReceivedLikeRouterFeature/ReceivedLikeRouter.swift
+++ Packages/BeMatch/Sources/ReceivedLikeRouterFeature/ReceivedLikeRouter.swift
@@ -1,6 +1,8 @@
+import BeMatch
+import BeMatchClient
 import ComposableArchitecture
-import ReceivedLikeSwipeFeature
 import MembershipFeature
+import ReceivedLikeSwipeFeature
 import SwiftUI
 
 @Reducer
@@ -8,21 +10,47 @@ public struct ReceivedLikeRouterLogic {
   public init() {}
 
   public enum State: Equatable {
-    case membership(MembershipLogic.State)
-    case swipe(ReceivedLikeSwipeLogic.State)
+    case loading
+    case membership(MembershipLogic.State = .init())
+    case swipe(ReceivedLikeSwipeLogic.State = .init())
   }
 
   public enum Action {
     case onTask
     case membership(MembershipLogic.Action)
     case swipe(ReceivedLikeSwipeLogic.Action)
+    case hasPremiumMembershipResponse(Result<BeMatch.HasPremiumMembershipQuery.Data, Error>)
+  }
+
+  @Dependency(\.bematch) var bematch
+  @Dependency(\.dismiss) var dismiss
+
+  enum Cancel {
+    case hasPremiumMembership
   }
 
   public var body: some Reducer<State, Action> {
     Reduce<State, Action> { state, action in
       switch action {
       case .onTask:
+        return .run { send in
+          for try await data in bematch.hasPremiumMembership() {
+            await send(.hasPremiumMembershipResponse(.success(data)))
+          }
+        } catch: { error, send in
+          await send(.hasPremiumMembershipResponse(.failure(error)))
+        }
+        .cancellable(id: Cancel.hasPremiumMembership, cancelInFlight: true)
+
+      case let .hasPremiumMembershipResponse(.success(data)):
+        state = data.hasPremiumMembership ? .swipe() : .membership()
         return .none
+
+      case .hasPremiumMembershipResponse(.failure):
+        return .run { _ in
+          await dismiss()
+        }
+
       default:
         return .none
       }
@@ -46,19 +74,25 @@ public struct ReceivedLikeRouterView: View {
   public var body: some View {
     SwitchStore(store) { initialState in
       switch initialState {
+      case .loading:
+        ProgressView()
+          .tint(Color.white)
+
       case .membership:
         CaseLet(
           /ReceivedLikeRouterLogic.State.membership,
-           action: ReceivedLikeRouterLogic.Action.membership,
-           then: MembershipView.init(store:)
+          action: ReceivedLikeRouterLogic.Action.membership,
+          then: MembershipView.init(store:)
         )
+
       case .swipe:
         CaseLet(
           /ReceivedLikeRouterLogic.State.swipe,
-           action: ReceivedLikeRouterLogic.Action.swipe,
-           then: ReceivedLikeSwipeView.init(store:)
+          action: ReceivedLikeRouterLogic.Action.swipe,
+          then: ReceivedLikeSwipeView.init(store:)
         )
       }
     }
+    .task { await store.send(.onTask).finish() }
   }
 }
