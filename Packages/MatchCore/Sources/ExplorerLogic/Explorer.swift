29bdd9eaeec05e5db2b9545fbe8a014c697e7f45
tomokisun <github@tomokisun.com>
2024-05-17 02:24:41 +0900

    feat: explorer logic

--- Packages/MatchCore/Sources/ExplorerLogic/Explorer.swift
+++ Packages/MatchCore/Sources/ExplorerLogic/Explorer.swift
@@ -1,4 +1,6 @@
 import AnalyticsClient
+import API
+import APIClient
 import ComposableArchitecture
 import SwiftUI
 
@@ -6,39 +8,45 @@ import SwiftUI
 public struct ExplorerLogic {
   public init() {}
 
-  public struct State: Equatable {
-    public var child = Child.State.loading
-
-    public init() {}
+  public enum State: Equatable {
+    case loading
+    case empty(ExplorerEmptyLogic.State = .init())
+    case content(ExplorerContentLogic.State)
   }
 
   public enum Action {
-    case child(Child.Action)
+    case onTask
+    case explorersResponse(Result<API.ExplorersQuery.Data, Error>)
+    case loading
+    case empty(ExplorerEmptyLogic.Action)
+    case content(ExplorerContentLogic.Action)
   }
 
+  @Dependency(\.api) var api
   @Dependency(\.analytics) var analytics
 
   public var body: some Reducer<State, Action> {
-    Scope(state: \.child, action: \.child, child: Child.init)
-  }
+    Scope(state: \.empty, action: \.empty, child: ExplorerEmptyLogic.init)
+    Scope(state: \.content, action: \.content, child: ExplorerContentLogic.init)
+    Reduce<State, Action> { state, action in
+      switch action {
+      case .onTask:
+        analytics.logScreen(screenName: "Explorer", of: self)
+        return .none
 
-  @Reducer
-  public struct Child {
-    public enum State: Equatable {
-      case loading
-      case empty(ExplorerEmptyLogic.State = .init())
-      case content(ExplorerContentLogic.State)
-    }
+      case let .explorersResponse(.success(data)):
+        let elements = data.explorers
+          .filter { !$0.users.isEmpty }
+          .sorted(by: { $0.order < $1.order })
+          .map(ExplorerContentSectionLogic.State.init(explorer:))
+        state = .content(
+          ExplorerContentLogic.State(uniqueElements: elements)
+        )
+        return .none
 
-    public enum Action {
-      case loading
-      case empty(ExplorerEmptyLogic.Action)
-      case content(ExplorerContentLogic.Action)
-    }
-
-    public var body: some Reducer<State, Action> {
-      Scope(state: \.empty, action: \.empty, child: ExplorerEmptyLogic.init)
-      Scope(state: \.content, action: \.content, child: ExplorerContentLogic.init)
+      default:
+        return .none
+      }
     }
   }
 }
