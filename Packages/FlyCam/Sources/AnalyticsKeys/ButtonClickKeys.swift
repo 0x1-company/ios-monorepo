40bfcb543ab7b718df681d1d649f8d226341fd63
tomokisun <github@tomokisun.com>
2024-05-17 16:26:23 +0900

    feat: delete flycam

--- Packages/FlyCam/Sources/AnalyticsKeys/ButtonClickKeys.swift
+++
@@ -1,8 +0,0 @@
-import AnalyticsClient
-
-public extension ButtonClickKeys {
-  var forceUpdate: String { "force_update" }
-  var close: String { "close" }
-  var notNow: String { "not_now" }
-  var delete: String { "delete" }
-}
--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -4,6 +4,4 @@ script_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd "$script_dir/.."
 
 echo $FILE_FIREBASE_STAGING | base64 --decode > App/BeMatch/Multiplatform/Staging/GoogleService-Info.plist
-echo $FILE_FIREBASE_STAGING | base64 --decode > App/FlyCam/Multiplatform/Staging/GoogleService-Info.plist
 echo $FILE_FIREBASE_PRODUCTION | base64 --decode > App/BeMatch/Multiplatform/Production/GoogleService-Info.plist
-echo $FILE_FIREBASE_PRODUCTION | base64 --decode > App/FlyCam/Multiplatform/Production/GoogleService-Info.plist
