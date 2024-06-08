7deb9082ba1f6300f7cdbf9564b6c3d0103157f1
tomokisun <github@tomokisun.com>
2024-06-08 10:51:51 +0900

    feat(trinket): how to locket link

---
+++ Packages/MatchCore/Sources/HowToLocketLinkLogic/HowToLocketLink.swift
@@ -0,0 +1,51 @@
+import AnalyticsClient
+import FeedbackGeneratorClient
+import AVKit
+import ComposableArchitecture
+
+@Reducer
+public struct HowToLocketLinkLogic {
+  public init() {}
+
+  public struct State: Equatable {
+    public let player: AVPlayer
+
+    public init() {
+      let url = URL(string: "https://storage.googleapis.com/bematch-production.appspot.com/public/trinket/how-to-invitation-link.MP4")!
+      player = AVPlayer(url: url)
+    }
+  }
+
+  public enum Action {
+    case onTask
+    case closeButtonTapped
+    case delegate(Delegate)
+    
+    public enum Delegate: Equatable {
+      case dismiss
+    }
+  }
+
+  @Dependency(\.analytics) var analytics
+  @Dependency(\.feedbackGenerator) var feedbackGenerator
+
+  public var body: some Reducer<State, Action> {
+    Reduce<State, Action> { state, action in
+      switch action {
+      case .onTask:
+        state.player.play()
+        analytics.logScreen(screenName: "HowToLocketLink", of: self)
+        return .none
+        
+      case .closeButtonTapped:
+        return .run { send in
+          await feedbackGenerator.impactOccurred()
+          await send(.delegate(.dismiss))
+        }
+        
+      default:
+        return .none
+      }
+    }
+  }
+}
