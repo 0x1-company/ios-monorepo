119cbdf183db4d0c57c343e85fb8fb35669ef6be
tomokisun <github@tomokisun.com>
2024-08-28 15:17:23 +0900

    fix: analytics (#1050)

--- Packages/MatchCore/Sources/AnalyticsKeys/UserPropertyKeys.swift
+++ Packages/MatchCore/Sources/AnalyticsKeys/UserPropertyKeys.swift
@@ -3,6 +3,5 @@ import AnalyticsClient
 public extension UserPropertyKeys {
   var id: String { "id" }
   var gender: String { "gender" }
-  var username: String { "username" }
   var onboardCompleted: String { "onboard_completed" }
 }
