3d3ddd71a22b7965d323d22c6f3821948a8e8162
tomokisun <github@tomokisun.com>
2024-08-28 15:30:07 +0900

    Revert "fix: analytics" (#1051)

--- Packages/MatchCore/Sources/AnalyticsKeys/UserPropertyKeys.swift
+++ Packages/MatchCore/Sources/AnalyticsKeys/UserPropertyKeys.swift
@@ -3,5 +3,6 @@ import AnalyticsClient
 public extension UserPropertyKeys {
   var id: String { "id" }
   var gender: String { "gender" }
+  var username: String { "username" }
   var onboardCompleted: String { "onboard_completed" }
 }
