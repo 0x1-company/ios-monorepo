6919d796f8c7728b20ed9d88ff56a43c7b99d8d8
tomokisun <github@tomokisun.com>
2024-09-17 17:08:39 +0900

    Revert "fix: notification center client" (#1122)

--- Packages/Dependencies/Sources/NotificationCenterClient/LiveKey.swift
+++ Packages/Dependencies/Sources/NotificationCenterClient/LiveKey.swift
@@ -5,14 +5,14 @@ import UIKit
 extension NotificationCenterClient: DependencyKey {
   public static let liveValue = Self(
     userDidTakeScreenshotNotification: {
-      AsyncStream(
+      await AsyncStream(
         NotificationCenter.default
           .notifications(named: UIApplication.userDidTakeScreenshotNotification)
           .map { _ in }
       )
     },
     didBecomeActiveNotification: {
-      AsyncStream(
+      await AsyncStream(
         NotificationCenter.default
           .notifications(named: UIApplication.didBecomeActiveNotification)
           .map { _ in }
